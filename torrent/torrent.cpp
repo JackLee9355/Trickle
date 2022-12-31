b3a8a06e6498852b27b2fe864d59bd9cbf4a06b2
Jack Lee <welsar55@gmail.com>
2022-12-31 14:34:10 -0600

    Make the GET request work

--- torrent/torrent.cpp
+++ torrent/torrent.cpp
@@ -41,21 +41,10 @@ int Torrent::announce() {
         throw "UDP announcing unsupported at this time";
     }
 
-    struct addrinfo hints;
     struct addrinfo *result, *rp;
 
-    // This may or may not be copied from the manpages... It's a real PITA
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
-    hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
-    hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
-    hints.ai_protocol = 0; // Any protocol
-    hints.ai_canonname = NULL;
-    hints.ai_addr = NULL;
-    hints.ai_next = NULL;
-
     std::cout << url.hostname.c_str() << " " << url.port.c_str() << std::endl;
-    int err = getaddrinfo("google.com", "80", &hints, &result); // getaddrinfo(url.hostname.c_str(), url.port.c_str(), &hints, &result);
+    int err = getaddrinfo(url.hostname.c_str(), url.port.c_str(), NULL, &result);
     if (err < 0) {
         std::cout << err << std::endl;
         throw "Error occurred while attempting to resolve domain name";
@@ -71,21 +60,19 @@ int Torrent::announce() {
 
         if (connect(sfd, rp->ai_addr, rp->ai_addrlen) < 0) {
             close(sfd);
-            freeaddrinfo(result);
-            throw "Error connecting to socket";
+            std::cout << "Error connecting to socket" << std::endl;
+            continue;
         }
 
         if (send(sfd, request.c_str(), strlen(request.c_str()), 0) < 0) {
             close(sfd);
-            freeaddrinfo(result);
-            throw "Error writing to socket";
+            std::cout << "Error writing to socket" << std::endl;
+            continue;
         }
 
-        char buf[2048];
-        memset(buf, 0, sizeof(buf));
-        std::cout << "Reading..." << std::endl;
-        int amtRead = read(sfd, buf, 1);
-        std::cout << buf << std::endl;
+        char buffer[2048] = { 0 };
+        int amountRead = read(sfd, buffer, 2048);
+        std::cout << buffer << std::endl;
 
         close(sfd);
         break;
@@ -117,8 +104,5 @@ std::string Torrent::buildAnnouncerRequest(struct UrlComponents *url) {
     request += "&left=" + std::to_string(1000); // TODO: Replace this with a actual value.
     request += " HTTP/1.1\r\n";
     request += "Host: " + url->hostname + ":" + url->port + "\r\n\r\n";
-
-    request = "GET / HTTP/1.1\r\nHost: google.com\r\n\r\n";
-    std::cout << request << std::endl;
     return request;
 }
